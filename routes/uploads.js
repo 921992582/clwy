b06f1bbe7689f7471b21222e1ca3ac8aed09f471
xiewei <921992582@qq.com>
2025-01-10 18:02:39 +0800

    feat:阿里云图片上传

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { success, failure } = require('../utils/responses');
-const { config, client, singleFileUpload } = require('../utils/aliyun');
-const { BadRequest } = require('http-errors')
-const { Attachment } = require('../models');
-const { v4: uuidv4 } = require('uuid');
+const {success, failure} = require('../utils/responses');
+const {config, client, singleFileUpload, multipleFilesUpload} = require('../utils/aliyun');
+const {BadRequest} = require('http-errors')
+const {Attachment} = require('../models');
+const {v4: uuidv4} = require('uuid');
 const moment = require('moment');
 
 /**
@@ -21,7 +21,6 @@ router.post('/aliyun', function (req, res) {
       if (!req.file) {
         return failure(res, new BadRequest('请选择要上传的文件。'));
       }
-
       // 记录附件信息
       await Attachment.create({
         ...req.file,
@@ -29,12 +28,45 @@ router.post('/aliyun', function (req, res) {
         fullpath: req.file.path + '/' + req.file.filename,
       })
 
-      success(res, '上传成功。', { file: req.file.url });
+      success(res, '上传成功。', {file: req.file.url});
     });
   } catch (error) {
     failure(res, error);
   }
 })
+
+// 多文件上传
+router.post('/aliyunMultiple', function (req, res) {
+    try {
+
+
+      multipleFilesUpload(req, res, async function (error) {
+        if (error) {
+          return failure(res, error);
+        }
+
+        if (req.files.length === 0) {
+          return failure(res, new BadRequest('请选择要上传的文件。'));
+        }
+        // 记录附件信息
+        req.files.map(async item => {
+          await Attachment.create({
+            ...item,
+            userId: req.userId,
+            fullpath: item.path + '/' + item.filename,
+          })
+
+        })
+
+
+        success(res, '上传成功。', {files: req.files});
+      });
+    } catch (error) {
+      failure(res, error);
+    }
+  }
+)
+
 
 /**
  * 获取直传阿里云 OSS 授权信息
@@ -53,7 +85,7 @@ router.get('/aliyun_direct', async function (req, res, next) {
     conditions:
       [
         ['content-length-range', 0, 5 * 1024 * 1024], // 限制上传文件的大小为：5MB
-        { bucket: client.options.bucket }, // 限制上传的 bucket
+        {bucket: client.options.bucket}, // 限制上传的 bucket
         ['eq', '$key', key], // 限制上传的文件名
         ['in', '$content-type', ['image/jpeg', 'image/png', 'image/gif', 'image/webp']], // 限制文件类型
       ],
